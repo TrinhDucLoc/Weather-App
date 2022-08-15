54e6408332f5b291987b5ca592ffc815acf49e0b
TrinhDucLoc <westudymore@gmail.com>
2022-08-15 17:08:17 +0700

    Update Color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import "./App.css"
 
 const api = {
   key: "1449899dd8f0daa2add312cccd60f861",
@@ -23,7 +24,8 @@ function App() {
         const data = await response.json();
         if(response.ok) {
           // setWeatherInfo(JSON.stringify(data));
-          setWeatherInfo(`${data.name}, ${data.sys.country}, ${data.weather[0].description}, ${data.main.temp}*C`)
+          setWeatherInfo(`${data.name}, ${data.sys.country},
+          ${data.weather[0].description}, ${data.main.temp}*C`)
         } else {
           setErrorMessage(data.message);
         }
@@ -36,25 +38,37 @@ function App() {
     fetchWeatherData();
   }, [searchCity])
   
-
   const handleSubmit = (e) => {
     e.preventDefault();
     setSearchCity(searchInput);
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='City' value={searchInput} onChange={(e) => setSearchInput(e.target.value)}></input>
-        <button>Search</button>
-      </form>
-
-      {loading ? (<div>Loading...</div>) : (
+    
+    <div id="container">
+      <div id="main">
         <>
-          {errorMessage ? (<div style={{color: "red"}}>{errorMessage}</div>) : (<div>{weatherInfo}</div>)}
+        <div id="form-search">
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='City' value={searchInput} onChange={(e) => setSearchInput(e.target.value)}></input>
+            <button class='btn-search'>Search</button>
+          </form>
+        </div>
+        
+        <div class="form-result">
+          {loading ? (<div>Loading...</div>) : (
+            <>
+              {errorMessage ? (<div style={{color: "red"}}>{errorMessage}</div>) : 
+              (<div>{weatherInfo}</div>)}
+            </>
+          )}
+        </div>
+        
         </>
-      )}
-    </>
+
+      </div>
+    </div>
+      
   )
 }
 
